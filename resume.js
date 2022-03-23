7b52869738a56326f3017d92d6a46145caf2a5a8
Naresh-cyber2505 <77671842+Naresh-cyber2505@users.noreply.github.com>
2022-03-24 01:58:31 +0530

    updated to v.10

---
+++ resume.js
@@ -0,0 +1,96 @@
+// show mneu?
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+// remove menu mobile
+
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+// darkTheme
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+
+
+// reduce sixe and print an a4 sheet 
+
+function scaleCv(){
+    document.body.classList.add('scale-cv')
+}
+
+
+
+
+// remove the size when cv download
+function removeScale(){
+    document.body.classList.remove('scale-cv')
+}
+
+
+let areaCv = document.getElementById('area-cv')
+
+// pdf genrate 
+
+let  resumeButton = document.getElementById('resume-button')
+
+
+
+
+
+
+function gererateResume(){
+    html2pdf(areaCv)
+}
+
+
+resumeButton.addEventListener('click', () =>{
+    scaleCv()
+
+    gererateResume()
+
+
+})
